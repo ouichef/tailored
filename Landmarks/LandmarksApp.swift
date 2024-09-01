34973bd08090d7a2c3aa735ceba25cd3056dc6ca
Roman <roman@Rs-MacBook-Pro.local>
2024-09-01 14:15:44 -0700

    make it so

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by R on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
